e00fabfd0a67b8285a40732504667e3973df5797
장혁_집 <reaper83@naver.com>
2017-11-16 01:41:00 +0900

    update sql

--- program/sql/v_photovoltaic_status.sql
+++ program/sql/v_photovoltaic_status.sql
@@ -1,7 +1,10 @@
 
-SELECT pt.*, cs.connector_seq, cs.channel,
+SELECT 
+	pt.*, 
+	cs.connector_seq, cs.channel, 
+	sb.target_name AS saltern_block_name,
       CONCAT("ch_",cs.channel) AS t_channel,
-      (SELECT target_name FROM connector WHERE connector_seq=cs.connector_seq) AS con_name,
+      (SELECT target_name FROM connector WHERE connector_seq=cs.connector_seq) AS connector_name,
       CASE 
       	WHEN cs.channel = 1
       	THEN (SELECT ch_1 FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
@@ -12,6 +15,22 @@ SELECT pt.*, cs.connector_seq, cs.channel,
 			WHEN cs.channel = 4
       	THEN (SELECT ch_4 FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
       END AS amp,
-      (SELECT v FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1) AS vol
-      FROM photovoltaic pt ,connector_structure cs
+      (SELECT v FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1) AS vol,
+      CASE 
+      	WHEN cs.channel = 1
+      	THEN (SELECT writedate FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
+			WHEN cs.channel = 2
+      	THEN (SELECT writedate FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
+			WHEN cs.channel = 3
+      	THEN (SELECT writedate FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
+			WHEN cs.channel = 4
+      	THEN (SELECT writedate FROM connector_data WHERE connector_seq = cs.connector_seq ORDER BY connector_data.connector_data_seq DESC LIMIT 1)
+      END AS writedate
+      
+      FROM photovoltaic pt
+      JOIN saltern_block sb
+      	ON sb.saltern_block_seq = pt.saltern_block_seq
+      ,connector_structure cs
+      
+      
        WHERE pt.photovoltaic_seq=cs.photovoltaic_seq  ORDER BY connector_seq,cs.channel

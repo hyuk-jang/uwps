8705f69db9593c141d5fbc2dff37e4a9c44a2ab7
WIN-60344L4E5F2\Administrator <hyuk.jang@smsoft.co.kr>
2018-04-06 15:52:57 +0900

    @ 커밋 로그

--- module/Package/wms/weather-device/vantagepro2/src/Control.js
+++ module/Package/wms/weather-device/vantagepro2/src/Control.js
@@ -89,8 +89,8 @@ class Control extends AbstDeviceClient {
    * @param {Buffer} data 명령 수행 결과 데이터
    */
   updateDcData(processItem, data){
-    // BU.CLIN('data');
-    // BU.logFile('receive data: ' + data.toString('hex'));
+    BU.CLIN('data');
+    BU.appendFile(`./log/vantage/data/${BU.convertDateToText(new Date(), '', 2)}.txt`, `${this.config.deviceInfo.target_id} : ${data.toString('hex')}`);
     const resultParsing = this.converter.parsingUpdateData(processItem.cmdList[processItem.currCmdIndex], data);
     // BU.CLI(resultParsing);
     if(resultParsing.eventCode === 'done'){

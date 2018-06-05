8cd5bf868ef7ffc955115cad481f519e51f63e12
장혁_집 <reaper83@naver.com>
2018-06-05 22:16:44 +0900

    @ electron module dependency 수정

--- module/Package/Electron/config/config.js
+++ module/Package/Electron/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'dddd1111',
-    // password: 'reaper83',
+    // password: 'dddd1111',
+    password: 'reaper83',
     database: 'pv_led',
   }
 };

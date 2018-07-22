e81c7608205867731627cb07618a8b4ea0946794
장혁_집 <reaper83@naver.com>
2018-07-23 02:00:27 +0900

    @ 공통 양식 신규 버젼 추가 (FaceBook Style)

--- module/Package/salternControl/EchoServer/src/Model.js
+++ module/Package/salternControl/EchoServer/src/Model.js
@@ -1,16 +1,20 @@
-'use strict';
-const _ = require('lodash');
+"use strict";
+const _ = require("lodash");
 
-const {BU} = require('base-util-jh');
+const { BU } = require("base-util-jh");
 
-const {BaseModel} = require('../../../../module/device-protocol-converter-jh');
-require('./define');
+const {
+  BaseModel
+} = require("../../../../module/device-protocol-converter-jh");
+require("./define");
 
-const baseModel = new BaseModel.Saltern({mainCategory: 'Saltern', subCategory: 'xbee'});
+const baseModel = new BaseModel.Saltern({
+  mainCategory: "Saltern",
+  subCategory: "xbee"
+});
 const deviceModel = baseModel.device;
 
-
-const map = require('../../config/map');
+const map = require("../../config/map");
 
 class Model {
   constructor() {
@@ -27,17 +31,16 @@ class Model {
           waterDoor: deviceModel.WATER_DOOR.STATUS.STOP,
           waterLevel: deviceModel.WATER_LEVEL.STATUS.ZERO,
           valve: deviceModel.VALVE.STATUS.UNDEF,
-          pump: deviceModel.PUMP.STATUS.OFF,
+          pump: deviceModel.PUMP.STATUS.OFF
         });
       });
     });
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   findRouter(xbeeApi_0x10) {
-
     let deviceId = xbeeApi_0x10.destination64;
 
     let router = _.find(this.routerList, {
@@ -47,87 +50,97 @@ class Model {
     return router;
   }
 
-
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   onData(xbeeApi_0x10) {
     BU.CLI(xbeeApi_0x10);
     let router = this.findRouter(xbeeApi_0x10);
     BU.CLI(router);
 
-    if (_.includes(router.targetId, 'R_G_')) {
+    if (_.includes(router.targetId, "R_G_")) {
       this.controlWaterdoor(xbeeApi_0x10);
       return this.getWaterdoor(xbeeApi_0x10);
-    } else if (_.includes(router.targetId, 'R_V_')) {
+    } else if (_.includes(router.targetId, "R_V_")) {
       this.controlValve(xbeeApi_0x10);
       return this.getValve(xbeeApi_0x10);
-    } else if(_.includes(router.targetId, 'R_GV_')){
+    } else if (_.includes(router.targetId, "R_GV_")) {
       this.controlValve(xbeeApi_0x10);
       return this.getGateValve(xbeeApi_0x10);
-    } else if (_.includes(router.targetId, 'R_P_')) {
+    } else if (_.includes(router.targetId, "R_P_")) {
       this.controlPump(xbeeApi_0x10);
       return this.getPump(xbeeApi_0x10);
     }
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   getWaterdoor(xbeeApi_0x10) {
     let router = this.findRouter(xbeeApi_0x10);
     let bufferHex = [0x23, 0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x30, 0x31];
     switch (router.waterDoor) {
-    case deviceModel.WATER_DOOR.STATUS.STOP:
-      bufferHex = bufferHex.concat([0x30, 0x30]);
-      break;
-    case deviceModel.WATER_DOOR.STATUS.OPEN:
-      bufferHex = bufferHex.concat([0x30, 0x32]);
-      break;
-    case deviceModel.WATER_DOOR.STATUS.CLOSING:
-      bufferHex = bufferHex.concat([0x30, 0x33]);
-      break;
-    case deviceModel.WATER_DOOR.STATUS.CLOSE:
-      bufferHex = bufferHex.concat([0x30, 0x34]);
-      break;
-    case deviceModel.WATER_DOOR.STATUS.OPENING:
-      bufferHex = bufferHex.concat([0x30, 0x35]);
-      break;
+      case deviceModel.WATER_DOOR.STATUS.STOP:
+        bufferHex = bufferHex.concat([0x30, 0x30]);
+        break;
+      case deviceModel.WATER_DOOR.STATUS.OPEN:
+        bufferHex = bufferHex.concat([0x30, 0x32]);
+        break;
+      case deviceModel.WATER_DOOR.STATUS.CLOSING:
+        bufferHex = bufferHex.concat([0x30, 0x33]);
+        break;
+      case deviceModel.WATER_DOOR.STATUS.CLOSE:
+        bufferHex = bufferHex.concat([0x30, 0x34]);
+        break;
+      case deviceModel.WATER_DOOR.STATUS.OPENING:
+        bufferHex = bufferHex.concat([0x30, 0x35]);
+        break;
     }
     // Level: 2, Salinity: 4, Batter: 4
-    bufferHex = bufferHex.concat([0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x31, 0x30, 0x2e, 0x32]);
+    bufferHex = bufferHex.concat([
+      0x30,
+      0x30,
+      0x30,
+      0x30,
+      0x30,
+      0x30,
+      0x31,
+      0x30,
+      0x2e,
+      0x32
+    ]);
     return {
-      'type': 144,
-      'remote64': xbeeApi_0x10.destination64,
-      'remote16': xbeeApi_0x10.destination16,
-      'receiveOptions': 1,
+      type: 144,
+      remote64: xbeeApi_0x10.destination64,
+      remote16: xbeeApi_0x10.destination16,
+      receiveOptions: 1,
       data: Buffer.from(bufferHex)
     };
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   getValve(xbeeApi_0x10) {
     BU.CLI(xbeeApi_0x10);
     let router = this.findRouter(xbeeApi_0x10);
     let bufferHex = [0x23, 0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x30, 0x32];
     switch (router.valve) {
-    case deviceModel.VALVE.STATUS.UNDEF:
-      bufferHex = bufferHex.concat([0x30, 0x30]);
-      break;
-    case deviceModel.VALVE.STATUS.CLOSE:
-      bufferHex = bufferHex.concat([0x30, 0x31]);
-      break;
-    case deviceModel.VALVE.STATUS.OPEN:
-      bufferHex = bufferHex.concat([0x30, 0x32]);
-      break;
-    case deviceModel.VALVE.STATUS.OPENING:
-      bufferHex = bufferHex.concat([0x30, 0x34]);
-      break;
-    case deviceModel.VALVE.STATUS.CLOSING:
-      bufferHex = bufferHex.concat([0x30, 0x35]);
-      break;
+      case deviceModel.VALVE.STATUS.UNDEF:
+        bufferHex = bufferHex.concat([0x30, 0x30]);
+        break;
+      case deviceModel.VALVE.STATUS.CLOSE:
+        bufferHex = bufferHex.concat([0x30, 0x31]);
+        break;
+      case deviceModel.VALVE.STATUS.OPEN:
+        bufferHex = bufferHex.concat([0x30, 0x32]);
+        break;
+      case deviceModel.VALVE.STATUS.OPENING:
+        bufferHex = bufferHex.concat([0x30, 0x34]);
+        break;
+      case deviceModel.VALVE.STATUS.CLOSING:
+        bufferHex = bufferHex.concat([0x30, 0x35]);
+        break;
     }
     // Level: 2
     bufferHex = bufferHex.concat([0x30, 0x30]);
@@ -138,43 +151,43 @@ class Model {
     // Batter: 4
     bufferHex = bufferHex.concat([0x31, 0x30, 0x2e, 0x32]);
     return {
-      'type': 144,
-      'remote64': xbeeApi_0x10.destination64,
-      'remote16': xbeeApi_0x10.destination16,
-      'receiveOptions': 1,
+      type: 144,
+      remote64: xbeeApi_0x10.destination64,
+      remote16: xbeeApi_0x10.destination16,
+      receiveOptions: 1,
       data: Buffer.from(bufferHex)
     };
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   getGateValve(xbeeApi_0x10) {
     BU.CLI(xbeeApi_0x10);
     let router = this.findRouter(xbeeApi_0x10);
     let bufferHex = [0x23, 0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x30, 0x32];
     switch (router.valve) {
-    case deviceModel.VALVE.STATUS.UNDEF:
-      bufferHex = bufferHex.concat([0x30, 0x30]);
-      break;
-    case deviceModel.VALVE.STATUS.CLOSE:
-      bufferHex = bufferHex.concat([0x30, 0x31]);
-      break;
-    case deviceModel.VALVE.STATUS.OPEN:
-      bufferHex = bufferHex.concat([0x30, 0x32]);
-      break;
-    case deviceModel.VALVE.STATUS.OPENING:
-      bufferHex = bufferHex.concat([0x30, 0x34]);
-      break;
-    case deviceModel.VALVE.STATUS.CLOSING:
-      bufferHex = bufferHex.concat([0x30, 0x35]);
-      break;
+      case deviceModel.VALVE.STATUS.UNDEF:
+        bufferHex = bufferHex.concat([0x30, 0x30]);
+        break;
+      case deviceModel.VALVE.STATUS.CLOSE:
+        bufferHex = bufferHex.concat([0x30, 0x31]);
+        break;
+      case deviceModel.VALVE.STATUS.OPEN:
+        bufferHex = bufferHex.concat([0x30, 0x32]);
+        break;
+      case deviceModel.VALVE.STATUS.OPENING:
+        bufferHex = bufferHex.concat([0x30, 0x34]);
+        break;
+      case deviceModel.VALVE.STATUS.CLOSING:
+        bufferHex = bufferHex.concat([0x30, 0x35]);
+        break;
     }
 
     // Level: 3 (138 데이터])
     let wl = _.random(80, 190);
     BU.CLI(_.subtract(200, wl));
-    let strWl = _.padStart(wl.toString(), 3, '0');
+    let strWl = _.padStart(wl.toString(), 3, "0");
     BU.CLI(strWl);
     let buf = Buffer.from(strWl);
     buf.forEach(currentItem => {
@@ -188,41 +201,41 @@ class Model {
     // Batter: 4
     bufferHex = bufferHex.concat([0x31, 0x30, 0x2e, 0x32]);
     return {
-      'type': 144,
-      'remote64': xbeeApi_0x10.destination64,
-      'remote16': xbeeApi_0x10.destination16,
-      'receiveOptions': 1,
+      type: 144,
+      remote64: xbeeApi_0x10.destination64,
+      remote16: xbeeApi_0x10.destination16,
+      receiveOptions: 1,
       data: Buffer.from(bufferHex)
     };
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   getPump(xbeeApi_0x10) {
     let router = this.findRouter(xbeeApi_0x10);
     let bufferHex = [0x23, 0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x30, 0x33];
     switch (router.pump) {
-    case deviceModel.PUMP.STATUS.OFF:
-      bufferHex = bufferHex.concat([0x30, 0x30]);
-      break;
-    case deviceModel.PUMP.STATUS.ON:
-      bufferHex = bufferHex.concat([0x30, 0x31]);
-      break;
+      case deviceModel.PUMP.STATUS.OFF:
+        bufferHex = bufferHex.concat([0x30, 0x30]);
+        break;
+      case deviceModel.PUMP.STATUS.ON:
+        bufferHex = bufferHex.concat([0x30, 0x31]);
+        break;
     }
     // Batter: 4
     bufferHex = bufferHex.concat([0x31, 0x30, 0x2e, 0x32]);
     return {
-      'type': 144,
-      'remote64': xbeeApi_0x10.destination64,
-      'remote16': xbeeApi_0x10.destination16,
-      'receiveOptions': 1,
+      type: 144,
+      remote64: xbeeApi_0x10.destination64,
+      remote16: xbeeApi_0x10.destination16,
+      receiveOptions: 1,
       data: Buffer.from(bufferHex)
     };
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   controlWaterdoor(xbeeApi_0x10) {
     let router = this.findRouter(xbeeApi_0x10);
@@ -249,7 +262,7 @@ class Model {
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   controlValve(xbeeApi_0x10) {
     let router = this.findRouter(xbeeApi_0x10);
@@ -273,11 +286,10 @@ class Model {
         }, _.random(3000, 5000));
       }
     }
-
   }
 
   /**
-   * @param {xbeeApi_0x10} xbeeApi_0x10 
+   * @param {xbeeApi_0x10} xbeeApi_0x10
    */
   controlPump(xbeeApi_0x10) {
     let router = this.findRouter(xbeeApi_0x10);
@@ -301,13 +313,7 @@ class Model {
         }, _.random(3000, 5000));
       }
     }
-
   }
-
-
-
-
 }
-
 
 module.exports = Model;
